bd71ac99d749fa5b2eb164ae6998eebb8d0c33ff
idlesong <idlesong@gmail.com>
2015-06-02 16:04:16 +0800

    aside add tag group

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ActiveRecord::Base
     following_ids = "SELECT followed_id FROM relationships
     WHERE  follower_id = :user_id"
     Post.where("user_id IN (#{following_ids})
-    OR user_id = :user_id", user_id: id)
+    OR user_id = :user_id", user_id: id).order(created_at: :desc)
   end
 
   # Follows a user.
